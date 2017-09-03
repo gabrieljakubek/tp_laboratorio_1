21c877c26f21bfd012346865a81450762763f847
gabrieljakubek <gabrieljakubek@gmail.com>
2017-09-03 18:49:56 -0300

    Estado final del Trabajo Practico 1

--- funciones.c
+++ funciones.c
@@ -1,1 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
 
+float funIngreso()
+{
+    float num;
+    printf("Ingrese un numero: ");
+    scanf("%f",&num);
+    system("cls");
+    return num;
+}
+
+float funSumar(float primernum,float segundonum)
+{
+    float resultado;
+    resultado = primernum + segundonum;
+    return resultado;
+}
+
+float funResta(float primernum,float segundonum)
+{
+    float resultado;
+    resultado = primernum - segundonum;
+    return resultado;
+}
+
+float funDividir (float primernum,float segundonum)
+{
+    float resultado;
+    resultado = primernum / segundonum;
+    return resultado;
+}
+
+float funMultiplicaion (float primernum,float segundonum)
+{
+    long double resultado;
+    resultado = primernum * segundonum;
+    return resultado;
+}
+
+float funFactorial (float primernum)
+{
+    int respuesta = 1;
+    int cont;
+    int validarpos;
+    int validarmenorigualdoce;
+    int validarcoma;
+    validarcoma = funValidarComa(primernum);
+    validarmenorigualdoce = funValidarMenorIgualdoce(primernum);
+    validarpos = funValidarPositivos(primernum);
+    if (validarcoma != 0 && validarmenorigualdoce != 0 && validarpos != 0)
+    {
+        for (cont = (int) primernum; cont > 1; cont--)
+        {
+            respuesta = respuesta * cont;
+        }
+    }
+    else
+    {
+        respuesta = 0;
+    }
+    return respuesta;
+}
+
+float funValidarCero (float primernum,float segundonum)
+{
+    int respuesta =0;
+    while (primernum != (float) 0 && segundonum != (float) 0)
+    {
+        respuesta  = 1;
+        break;
+    }
+    return respuesta;
+}
+
+float funValidarPositivos (float num)
+{
+    int retorno = 0;
+    if (num >= (float)0)
+    {
+        retorno = 1;
+    }
+    return retorno;
+}
+
+float funValidarMenorIgualdoce (float num)
+{
+    int retorno = 0;
+    if (num <=12)
+    {
+        retorno = 1;
+    }
+    return retorno;
+}
+
+float funValidarComa (float num)
+{
+    int retorno;
+    if (num - (int) num == 0)
+    {
+        retorno = 1;
+    }
+    else
+    {
+        retorno = 0;
+    }
+    return retorno;
+}
--- main.c
+++ main.c
@@ -4,44 +4,120 @@
 
 int main()
 {
+    float operA = 0;
+    float operB = 0;
+    float suma = 0;
+    float resta = 0;
+    float multiplicacion = 0;
+    float division = 0;
+    int factorial = 0;
+    int valdivision;
     char seguir='s';
     int opcion=0;
 
     while(seguir=='s')
     {
-        printf("1- Ingresar 1er operando (A=x)\n");
-        printf("2- Ingresar 2do operando (B=y)\n");
-        printf("3- Calcular la suma (A+B)\n");
-        printf("4- Calcular la resta (A-B)\n");
-        printf("5- Calcular la division (A/B)\n");
-        printf("6- Calcular la multiplicacion (A*B)\n");
-        printf("7- Calcular el factorial (A!)\n");
+        printf("1- Ingresar 1er operando (A=%.3f)\n",operA);
+        printf("2- Ingresar 2do operando (B=%.3f)\n",operB);
+        printf("3- Calcular la suma (A+B) = %.3f\n",suma);
+        printf("4- Calcular la resta (A-B) = %.3f\n",resta);
+        printf("5- Calcular la division (A/B) = %.3f\n",division);
+        printf("6- Calcular la multiplicacion (A*B) = %.3f\n",multiplicacion);
+        printf("7- Calcular el factorial (A!) = %d\n",factorial);
         printf("8- Calcular todas las operacione\n");
         printf("9- Salir\n");
-
+        printf("Ingrese la opcion que desea usar: ");
         scanf("%d",&opcion);
 
         switch(opcion)
         {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            case 7:
-                break;
-            case 8:
-                break;
-            case 9:
-                seguir = 'n';
-                break;
+        case 1:
+            operA = funIngreso();
+            break;
+        case 2:
+            operB = funIngreso();
+            break;
+        case 3:
+            suma = funSumar(operA, operB);
+            printf("La suma es de: %f\n", suma);
+            system("pause");
+            system("cls");
+            break;
+        case 4:
+            resta = funResta(operA, operB);
+            printf("La resta es de: %f\n", resta);
+            system("pause");
+            system("cls");
+            break;
+        case 5:
+            valdivision = funValidarCero(operA, operB);
+            if (valdivision != 0)
+            {
+                division = funDividir(operA, operB);
+                printf("La division es de: %.3f\n", division);
+                system("pause");
+                system("cls");
+            }
+            else
+            {
+                printf("Error!!!! No se puede realizar la operacion \n");
+                system("pause");
+                system("cls");
+            }
+            break;
+        case 6:
+            multiplicacion = funMultiplicaion(operA, operB);
+            printf("La multiplicacion es de: %3f\n", multiplicacion);
+            system("pause");
+            system("cls");
+            break;
+        case 7:
+            factorial= funFactorial(operA);
+            if(factorial != 0)
+            {
+                printf("El factorial de %.f es de: %d\n", operA,factorial);
+                system("pause");
+                system("cls");
+            }
+            else
+            {
+                printf("Error!!! No se puede sacar el factorialcon de ese numero! \n");
+                system("pause");
+                system("cls");
+            }
+            break;
+        case 8:
+            suma = funSumar(operA, operB);
+            printf("La suma es de: %f\n", suma);
+            resta = funResta(operA, operB);
+            printf("La resta es de: %f\n", resta);
+            valdivision = funValidarCero(operA, operB);
+            if (valdivision != 0)
+            {
+                division = funDividir(operA, operB);
+                printf("La division es de: %.3f\n", division);
+            }
+            else
+            {
+                printf("Error!!!! No se puede realizar la operacion \n");
+            }
+            multiplicacion = funMultiplicaion(operA, operB);
+            printf("La multiplicacion es de: %f\n", multiplicacion);
+            factorial= funFactorial(operA);
+            if(factorial != 0)
+            {
+                printf("El factorial de %.f es de: %d\n", operA,factorial);
+            }
+            else
+            {
+                printf("Error!!! No se puede sacar el factorialcon de ese numero! \n");
+            }
+            system("pause");
+            system("cls");
+            break;
+        case 9:
+            seguir = 'n';
+            break;
         }
     }
 
