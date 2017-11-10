2216d871a19def0f6f7556e1018751978c4ff4e0
gabrieljakubek <gabrieljakubek@gmail.com>
2017-11-10 11:17:28 -0300

    Arraylist Terminado

---
+++ TP_04/arraylist/examples/example_4/src/main.c
@@ -0,0 +1,56 @@
+/*
+    utest example : Unit test examples.
+    Copyright (C) <2016>  <Mauricio Davila>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../testing/inc/main_test.h"
+#include "../inc/ArrayList.h"
+#include "../inc/Employee.h"
+
+
+
+int main(void)
+{
+        startTesting(1); //New Array
+        startTesting(2); //Add
+        startTesting(3); //Delere Array
+        startTesting(4); //Len
+        startTesting(5); //Get
+        startTesting(6); //Contain
+        startTesting(7); //Set
+        startTesting(8); //Remove
+        startTesting(9); //Clear
+        startTesting(10); //Clone
+        startTesting(11); //Push
+        startTesting(12); //Index Of
+        startTesting(13); //Is Empty
+        startTesting(14); //Pop
+        startTesting(15); //SubList
+        startTesting(16); //Contains All
+        startTesting(17); //Sort
+
+
+    return 0;
+}
+
+
+
+
+
